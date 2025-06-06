7ee8e9bebea7d7cd02ab0eca0aca6f5879adc271
Marco Lecce <marco.lecce@skytv.it>
2025-06-06 15:26:04 +0200

    append hello+datetime on a log file

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+import os
+from datetime import datetime
+
 if __name__ == "__main__":
     print("hello!")
+    with open("logs/foo.txt","a+") as f:
+        f.write("hello?"+ str(datetime.now())+"\n")
