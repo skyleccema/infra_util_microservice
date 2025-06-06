949b92377dcfd1ff845ca0e8a50d218710a5eaae
Marco Lecce <marco.lecce@skytv.it>
2025-06-06 15:41:28 +0200

    added an example of python script that prints at login and recall hello + current datetime

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from datetime import datetime
 
 if __name__ == "__main__":
     print("hello!")
-    with open("logs/foo.txt","a+") as f:
+    with open("/home/ubuml/github_repos/infra_util_microservice/logs/foo.txt","a+") as f:
         f.write("hello?"+ str(datetime.now())+"\n")
