8c5801a5145e27321eaa3edcad522a8fa2293fe8
Marco Lecce <marco.lecce@skytv.it>
2025-06-11 11:58:54 +0200

    flask hello web app

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dockerize infra-util as Microservice
+
+## Step 1
+Dockerize a Flask application
+
+## Step 2
+add an "hello" route
---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask, make_response
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return make_response("Hello World")
