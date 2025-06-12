931b8414ccd3a86556068cf77593a5b5b69ad395
Marco Lecce <marco.lecce@skytv.it>
2025-06-12 15:00:00 +0200

    print env type instead of hello

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,11 @@
 
 FROM python:3.13
 
-RUN mkdir app infra_utils
-COPY ./.env ./app
-COPY ./app ./app
-COPY ./infra_utils ./infra_utils
-RUN pip install flask ./infra_utils/v2.0/dist/infra_utils_sqlalchemy_2.0-0.0.2-py3-none-any.whl
+RUN mkdir -p /app/infra_utils
+COPY ./env /app
+COPY ./app /app
+COPY ./infra_utils /app/infra_utils
+RUN pip install flask python-dotenv /app/infra_utils/v2.0/dist/infra_utils_sqlalchemy_2.0-0.0.2-py3-none-any.whl
+WORKDIR /app
 
 CMD ["flask", "run", "--host=0.0.0.0"]
--- app/app.py
+++ app/app.py
@@ -1,11 +1,15 @@
+from os import environ
 from flask import Flask, make_response
 from infra_utils.QueryInfradb import (fetch_rack_slot_type_by_project,
                                       fetch_rack_slot_type_by_project_grouped_by_rack,
                                       fetch_rack_slot_by_project_and_type_grouped_by_rack)
+from settings import ENV
+
 app = Flask(__name__)
+app.config.from_pyfile('settings.py')
 
 def hello():
-    return "Hello vov"
+    return ENV
 
 @app.route("/")
 def hello_world():
