6e676839f4c12350c868df5c119fc0a1c27535e1
Marco Lecce <marco.lecce@skytv.it>
2025-06-13 17:30:31 +0200

    chore: added support for all functions except for update and put methods.
    not sure of correct working endpoints fetch_slots_versions_with_dinamic_filter

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ RUN mkdir -p /app/infra_utils
 COPY ./env /app
 COPY ./app /app
 COPY ./infra_utils /app/infra_utils
-RUN pip install flask python-dotenv /app/infra_utils/v2.0/dist/infra_utils_sqlalchemy_2.0-0.0.2-py3-none-any.whl
+RUN pip install flask flask_cors python-dotenv /app/infra_utils/v2.0/dist/infra_utils_sqlalchemy_2.0-0.0.2-py3-none-any.whl
 WORKDIR /app
 
 CMD ["flask", "run", "--host=0.0.0.0"]
--- app/app.py
+++ app/app.py
@@ -1,10 +1,20 @@
 from os import environ
 from flask import Flask, make_response
-from infra_utils.QueryInfradb import (available_slots,
+from flask_cors import CORS
+from infra_utils.QueryInfradb import (query_stb_info,
+                                      get_stb_status_broken,
+                                      update_broken_status,
+                                      get_broken_from_rack,
+                                      query_stb_project_info,
+                                      get_all_stb,
+                                      put_stb,
+                                      get_rack_slot_by_ip,
+                                      available_slots,
                                       get_auto_reboot,
                                       get_ip,
                                       get_stbs_by_project,
                                       fetch_slots_versions,
+                                      fetch_slots_versions_with_dinamic_filter,
                                       fetch_rack_slot_type_by_project,
                                       fetch_rack_slot_by_project_and_type,
                                       fetch_rack_slot_type_by_project_grouped_by_rack,
@@ -16,22 +26,88 @@ load_dotenv('.env', verbose=True)
 
 app = Flask(__name__)
 
+# CORS 
+CORS(app)
+# successivamente accetteremo richieste da subset di IP
+
 def hello():
     return environ.get("ENV")
 
 @app.route("/")
 def hello_world():
+    # try-except log e tutte le info base su oggetti Flask
+    # gestione log Flask con sua libreria ma devo farlo bene
+    # nell'except importo la libreria traceback
+    # nel log uso la funz di tracecback.formatexec
+    # così ho tutto lo stack printato e loggato :)
     return make_response(hello())
 
-# missing endpoints  
-# fetch_slots_versions_with_dinamic_filter
-# get_auto_reboot (read before creating the endpoint)
-# and from get_rack_slot_by_ip upwards
+# missing endpoints
+# TBT api_fetch_slots_versions_with_dinamic_filter
+
+
+# TBD! test fetch_slots_versions_with_dinamic_filter to pick CERRI Slot 16
+# problem: cannot guess how to write country code
+# tested with http://127.0.0.1:5000/fetch_slots_versions_with_dinamic_filter/CERRI/Llama/ITA/4.0 META3/QS036.018.00U returns empty json
+@app.route("/fetch_slots_versions_with_dinamic_filter/<proj>/<platform>/<country>/<rack_name>/<slot_version>")
+def api_fetch_slots_versions_with_dinamic_filter(proj, platform, country, rack_name, slot_version):
+    return make_response(fetch_slots_versions_with_dinamic_filter(proj, platform, country, rack_name, slot_version))
+
+# tested with http://127.0.0.1:5000/fetch_slots_versions_with_dinamic_filter/CERRI or PCC returns None
+@app.route("/fetch_slots_versions_with_dinamic_filter/<proj>")
+def api_fetch_slots_versions_with_dinamic_filter_none(proj):
+    result = make_response(str(fetch_slots_versions_with_dinamic_filter(proj, None, None, None, None))) 
+    return result# result if result is not None else make_response(str(result))
+
+# tested with http://localhost:5000/query_stb_info/10.170.0.199/4
+@app.route("/query_stb_info/<ip>/<slot>")
+def api_query_stb_info(ip, slot):
+    return make_response(str(query_stb_info(ip, slot)))
+
+# tested with http://localhost:5000/get_stb_status_broken/10.170.0.199/4
+@app.route("/get_stb_status_broken/<ip>/<slot>")
+def api_get_stb_status_broken(ip, slot):
+    return make_response(str(get_stb_status_broken(ip, slot)))
+
+# @app.route("/update_broken_status/<ip>/<slot>/<broken>")
+# def api_update_broken_status(ip, slot, broken):
+#     update_broken_status(ip, slot, broken)
+
+# tested with http://localhost:5000/get_broken_from_rack/10.170.0.199
+@app.route("/get_broken_from_rack/<ip_rack>")
+def api_get_broken_from_rack(ip_rack):
+    return get_broken_from_rack(ip_rack)
+
+@app.route("/query_stb_project_info/<ip>/<slot>")
+def api_query_stb_project_info(ip, slot):
+    return make_response(query_stb_project_info(ip, slot))
+
+# tested with http://127.0.0.1:5000/get_all_stb
+@app.route("/get_all_stb")
+def api_get_all_stb():
+    return make_response(str(get_all_stb()))
+
+# tested with http://127.0.0.1:5000/put_stb/?
+# ask francesco
+# @app.route("/put_stb/<stb>")
+# def api_put_stb(stb):
+#     return make_response(put_stb(stb))
+
+# tested with http://127.0.0.1:5000/get_rack_slot_by_ip/10.170.0.177
+@app.route("/get_rack_slot_by_ip/<ip>")
+def api_get_rack_slot_by_ip(ip):
+    result = get_rack_slot_by_ip(ip)
+    return make_response(result) if result else make_response(str(result))
 
 # tested with http://127.0.0.1:5000/available_slots/CERRI/Llama
 @app.route("/available_slots/<proj>/<typ>")
 def api_available_slots(proj, typ):
     return make_response(str(available_slots(proj,typ)))
+
+# tested with http://127.0.0.1:5000/get_auto_reboot
+@app.route("/get_auto_reboot")
+def api_get_auto_reboot():
+    return make_response(str(get_auto_reboot()))
 
 # tested with http://127.0.0.1:5000/get_ip/3/STHD 06/10.170.1.71
 @app.route("/get_ip/<slot>/<server>/<ip>")
@@ -61,7 +137,7 @@ def api_fetch_rack_slot_by_project_and_type(proj, typ):
     return fetch_rack_slot_by_project_and_type(proj,typ)
 
 
-# tested with http://127.0.0.1:5000/fetch_rack_slot_by_project_grouped_by_rack/PCC but also CERRI
+# tested with http://127.0.0.1:5000/fetch_rack_slot_type_by_project_grouped_by_rack/PCC but also CERRI
 @app.route("/fetch_rack_slot_type_by_project_grouped_by_rack/<proj>")
 def api_fetch_rack_slot_type_by_project_grouped_by_rack(proj):
     # INITIALIZE CONNECTION TO DB and FETCH RSL info
