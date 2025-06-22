f078639c7286acb25b12268025b2b791a6748b44
Marco Lecce <marco.lecce@skytv.it>
2025-06-22 23:03:19 +0200

    poc: created an equivalent marshalled output for similar output for function "fetch_rack_slot_by_project_and_type_grouped_by_rack"

--- poc/app.py
+++ poc/app.py
@@ -1,35 +1,38 @@
 from flask import Flask
-from flask_restx import Api, Resource, fields
-
+from flask_restx import Api, Resource, fields, marshal
 app = Flask(__name__)
 api = Api(app)
-
-# Define the model for the dictionary
-dictionary_model = api.model('Dictionary', {
-    'key': fields.String(required=True, description='The key of the dictionary'),
-    'value': fields.String(required=True, description='The value of the dictionary')
+# Step 3: Define Models
+# Model for the inner dictionary
+field_model = api.model('Field', {
+    'name': fields.String(required=True, description='The name field')
 })
-
-# Define the resource and the endpoint
-@api.route('/dictionaries')
-class DictionaryList(Resource):
-    def get(self):
-        """
-        Retrieve a list of dictionaries
-        """
-        dictionaries = get_dictionaries()
-        # return api.marshal(dictionaries, fields.List(fields.Nested(dictionary_model)))
-        return api.marshal(dictionaries, dictionary_model)#for i in dictionary_model: fields.Nested(i))
-
-def get_dictionaries():
-    """
-    Function to retrieve a list of dictionaries
-    """
-    return [
-        {'key': 'key1', 'value': 'value1'},
-        {'key': 'key2', 'value': 'value2'},
-        {'key': 'key3', 'value': 'value3'}
+# Model for the outer dictionary
+group_model = api.model('Group', {
+    'field1': fields.String(required=True, description='The first field'),
+    'fields2': fields.List(fields.Nested(field_model), description='List of fields2')
+})
+# Model for the main structure
+main_model = api.model('Main', {
+    'group': fields.List(fields.Nested(group_model), description='List of groups')
+})
+# Sample data
+data = {
+    "group": [
+        {
+            "field1": "val1",
+            "fields2": [
+                {"name": "value11"},
+                {"name": "value12"}
+            ]
+        }
     ]
-
-if __name__ == "__main__":
+}
+# Step 4: Create a Resource
+@api.route('/data')
+class DataResource(Resource):
+    def get(self):
+        # Step 5: Marshal the data
+        return marshal(data, main_model), 200
+if __name__ == '__main__':
     app.run(debug=True)
